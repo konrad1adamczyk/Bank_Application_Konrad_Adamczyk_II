f3855bb8702e9d4d0dbe209b9b376021807db2b3
Konrad Adamczyk <konrad1adamczyk@gmail.com>
2016-01-02 19:13:06 +0100

    tast 6

--- src/com/luxoft/bankapp/tests/TestSerialization.java
+++ src/com/luxoft/bankapp/tests/TestSerialization.java
@@ -1,63 +1,20 @@
 package com.luxoft.bankapp.tests;
 
-import com.luxoft.bankapp.ecxeptions.ClientExistsException;
-import com.luxoft.bankapp.ecxeptions.ClientNotExistsException;
 import com.luxoft.bankapp.model.*;
-import com.luxoft.bankapp.service.BankApplication;
 import com.luxoft.bankapp.service.BankFeedService;
-import com.luxoft.bankapp.service.BankService;
 import com.luxoft.bankapp.service.BankServiceImpl;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
-
-import static org.junit.Assert.assertEquals;
-
 
 /**
- * Created by Konrad on 2016-01-01.
+ * Created by Konrad on 2016-01-02.
  */
 public class TestSerialization {
-
-    BankService bService;
-    Bank bank;
-    Client client;
-
-//    @Before
-//    public void peeperTest() throws ClientExistsException {
-//
-//        BankApplication bankApp = new BankApplication();
-//        bank = new Bank("MyBank");
-//        bankApp.initialize(bank);
-//
-//        bService = new BankServiceImpl();
-//        client = null;
-//    }
-//
-//    @Test
-//    public void testAObjectSerialize() throws ClientNotExistsException {
-//        bService.saveClient(bank.getClient(clientName));
-//    }
-//
-//    @Test
-//    public void testBObjectDeserialize() throws ClientNotExistsException, ClientExistsException {
-//        bService.removeClient(bank, bank.getClient(clientName));
-//        assertEquals(null, bank.getClient(clientName));
-//        client = bService.loadClient(clientObject);
-//        bService.addClient2(bank, client);
-//        assertEquals(bank.getClient(clientName), client);
-//        client.printReport();
-//    }
-
     public static void main(String[] args) {
 
         BankFeedService bankFeedService = new BankFeedService();
         bankFeedService.loadFeed();
         BankServiceImpl bankService = new BankServiceImpl();
 
-        Client client1 = new Client("Konradek","Krakow","franek@gmail.com","123456854", Gender.MALE,1000);
+        Client client1 = new Client("Konradek","Krakow","Konradek@gmail.com","123456854", Gender.MALE, 1000);
 
         Account savingAccount = new SavingAccount(300);
         Account checkingAccount = new CheckingAccount(300);
@@ -68,5 +25,4 @@ public class TestSerialization {
         bankService.saveClient(client1);
         System.out.println(bankService.loadClient());
     }
-
 }
